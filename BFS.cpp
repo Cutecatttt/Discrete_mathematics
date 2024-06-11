af1190288b876010714b68d14cf49d9e3f1d809f
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-06-11 10:41:52 +0700

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -7,7 +7,7 @@ bool visited[1001];
 
 void inp(){
     cin >> n >> m;
-    for(int i = 1; i < m; i++){
+    for(int i = 0; i < m; i++){
         int x, y; cin >> x >> y;
         a[x].push_back(y);
         a[y].push_back(x);
