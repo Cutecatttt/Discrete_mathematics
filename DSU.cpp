20ad090ece8004f3950ce139d5ba8566ffa83da5
Kiêm Khang <150389500+Cutecatttt@users.noreply.github.com>
2024-06-14 01:59:09 +0700

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int perent[100005], n;
+int parent[100005], size_set[100005], n;
 
 //Đặt nó là cha của chính nó
 void make_set(){
     for(int i = 1; i <= n; i++){
         parent[i] = i;
+        size_set[i] = 1;
     }
 }
 
 //Tìm cha của đỉnh v
-void find_parent(int v){
+int find_parent(int v){
     if(v == parent[v]) return v;
     return parent[v] = find_parent(parent[v]);
 }
@@ -20,7 +21,11 @@ void find_parent(int v){
 void union_set(int a, int b){
     a = find_parent(a);
     b = find_parent(b);
-    if(a != b) parent[b] =a;
+    if(a != b){
+        if(size_set[a] < size_set[b]) swap(a, b);
+        parent[b] = a;
+        size_set[a] += size_set[b];
+    }
 }
 
 int main() {
